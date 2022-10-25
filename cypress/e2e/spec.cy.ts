3a47b340ebe887558320e7f716376b466a81c9be
hacer7 <74371518+hacer7@users.noreply.github.com>
2022-10-25 14:09:28 +0300

    update

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,7 +2,7 @@
 
 describe('Pokedex e2e test', () => {
   beforeEach(() =>{
-    cy.visit('http://localhost:3002')
+    cy.visit('http://localhost:3000')
   })
 
   it('input test', () => {
@@ -29,7 +29,33 @@ describe('Pokedex e2e test', () => {
     cy.contains('vulpix').should('exist')
   })
 
-  it.only('pokemon by type test', () => {
+  it('pokes by type test', () => {
+    cy.get('.ant-btn').should('be.visible').click()
+    cy.get('.ant-popover-inner-content').contains('psychic').should('be.visible').click()
+    cy.get('.ant-input').should('not.exist')
+    cy.get('.ant-btn').should('have.text', 'psychic')
+    cy.wait(2000)
+    cy.get('.style_container__puRhW > div').contains('psychic').should('exist')
 
+    cy.get('.ant-btn').should('be.visible').click()
+    cy.get('.ant-popover-inner-content').contains('clear').should('be.visible').click()
+    cy.get('.ant-btn').should('have.text', 'Type Filter')
+    cy.get('.ant-input').should('exist')
+
+  })
+
+  it('fav pokemons test', () => {
+    cy.get(':nth-child(1) > .style_heart__cECPA').should('be.visible').click()
+    cy.get(':nth-child(5) > .style_heart__cECPA').should('be.visible').click()
+    cy.get(':nth-child(7) > .style_heart__cECPA').should('be.visible').click()
+    cy.get(':nth-child(9) > .style_heart__cECPA').should('be.visible').click()
+
+    cy.get('[href="/favourites"]').should('be.visible').click()
+    cy.url().should('eq', 'http://localhost:3000/favourites')
+    cy.wait(1000)
+    cy.contains('charmeleon').should('be.visible')
+
+    cy.get('[href="/"]').should('be.visible').click()
+    cy.url().should('eq', 'http://localhost:3000/')
   })
 })
